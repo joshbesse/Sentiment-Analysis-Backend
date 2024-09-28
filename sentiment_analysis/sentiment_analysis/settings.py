ba9a36af5a5892b66b19a2430b4359847361dbb7
joshbesse <joshua.besse3@gmail.com>
2024-09-28 19:52:38 -0400

    updated settings.py

--- sentiment_analysis/sentiment_analysis/settings.py
+++ sentiment_analysis/sentiment_analysis/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parents
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'sentiment_analysis.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
