ae5da6abd6a0a53f0ef66840d150ae29271704dd
joshbesse <joshua.besse3@gmail.com>
2024-09-30 15:03:52 -0400

    updated views

--- sentiment_analysis/analyze/views.py
+++ sentiment_analysis/analyze/views.py
@@ -7,26 +7,31 @@ from .serializers import AnalysisSerializer
 # Create your views here.
 facade = SentimentAnalysisFacade()
 
-@api_view(["POST"])
+@api_view(["GET", "POST"])
 def analyze_text(request):
-    text = request.data.get("text")
-    analyzer_type = request.data.get("analyzer_type", "basic")
-    print(analyzer_type)
+    if request.method == "POST":
+        text = request.data.get("text")
+        analyzer_type = request.data.get("analyzer_type", "basic")
+        print(analyzer_type)
 
-    facade.select_analyzer(analyzer_type)
-    result = facade.analyze_text(text)
+        facade.select_analyzer(analyzer_type)
+        result = facade.analyze_text(text)
 
-    analysis_result = Analyze.objects.create(
-        analyzer=analyzer_type,
-        text=text, 
-        sentiment=result.get_sentiment(), 
-        score=result.get_score()
-    )
+        analysis_result = Analyze.objects.create(
+            analyzer=analyzer_type,
+            text=text, 
+            sentiment=result.get_sentiment(), 
+            score=result.get_score()
+        )
 
-    return Response({
-        'sentiment': result.get_sentiment(),
-        'score': result.get_score()
-    })
+        return Response({
+            'sentiment': result.get_sentiment(),
+            'score': result.get_score()
+        })
+    elif request.method == "GET":
+        return Response({
+            'message': 'Please send a POST request with "text" and "analyzer_type".'
+        })
 
 @api_view(["GET"])
 def get_analysis_history(request):
