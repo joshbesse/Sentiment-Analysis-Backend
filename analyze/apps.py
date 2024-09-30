cb83c5d32e8c7b8f0412911aaf724cd8c9d18aec
joshbesse <joshua.besse3@gmail.com>
2024-09-30 17:59:19 -0400

    simplify project structure and fix module paths

--- analyze/apps.py
+++ analyze/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AnalyzeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sentiment_analysis.analyze'
+    name = 'analyze'
--- settings.py
+++ settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sentiment_analysis.analyze',
+    'analyze',
     'rest_framework',
     'corsheaders',
 ]
