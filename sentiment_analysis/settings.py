f2e1df9efffa11b8c2c70998c77039f6b394b82e
joshbesse <joshua.besse3@gmail.com>
2024-09-30 22:33:16 -0400

    updated INSTALLED_APPS

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'analyze',
+    'sentiment_analysis.analyze',
     'rest_framework',
     'corsheaders',
 ]
