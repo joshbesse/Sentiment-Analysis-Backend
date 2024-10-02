0323a3288fb668c37f4a01f4631479a3f5d4ee77
joshbesse <joshua.besse3@gmail.com>
2024-10-02 14:14:44 -0400

    updated urls

--- analyze/urls.py
+++ analyze/urls.py
@@ -4,5 +4,6 @@ from .views import analyze_text, get_analysis_history, get_task_status
 urlpatterns = [
     path('analyze/', analyze_text, name='analyze_text'),
     path('history/', get_analysis_history, name='get_analysis_history'),
-    path('task/<str:task_id>/', get_task_status, name='get_task_status'),  # Task status endpoint
+    path('', analyze_text, name='home'),
+    path('status/<str:task_id>/', get_task_status, name='get_task_status'),
 ]
